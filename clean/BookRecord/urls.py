c601b3304e4bce53d97a457fe6461665a90851dd
jaliaga_cenco <jose.aliaga@externos-ar.cencosud.com>
2022-06-19 12:14:11 -0300

    add book update

--- clean/BookRecord/urls.py
+++ clean/BookRecord/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('book/add/', views.Create_book.as_view(), name='create_book'),
     path('book/list/', views.View_books.as_view(), name='list_books'),
     path('book/rm/<pk>', views.Delete_book.as_view(), name='delete_book'),
+    path('book/<pk>', views.Detail_book.as_view(), name='detail_book'),
 ]
